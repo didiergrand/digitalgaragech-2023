8293c872e5ca6bd93c47326edf29ee11d41f4fd3
Didier.Grand <didier.grand@scott-sports.com>
2023-03-20 09:14:10 +0100

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,44 @@
 import '@component/styles/tailwind.css'
 import '@component/styles/globals.scss'
-// import the Head component for appending elements to the head of the page
+import Script from 'next/script'
 import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Head>
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
