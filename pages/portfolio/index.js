3d500db9ed32ce9ac58509609b12dee10eaff34f
Didier.Grand <didier.grand@scott-sports.com>
2023-06-15 09:06:48 +0200

    Add link

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -15,7 +15,7 @@ const Portfolio = () => {
             <p>Je vous invite à découvrir une sélection de mes travaux personnels en développement front-end et web design.</p>
             <p>Web designer et développeur front-end chez SCOTT Sports, je réalise également des projets personnels et propose mes services aux entreprises et organisations qui cherchent à créer ou améliorer leur présence en ligne.</p>
             <p>Je suis particulièrement actif pour la création de sites web à Châtel-St-Denis, et dans la région. Ces dernières années j’ai réalisé les sites pour les communes de Semsales, Saint-Martin, Attalens, Bossonnens et Granges Veveyse. J’ai eu également eu le plaisir de réaliser plusieurs sites pour les girons de Musiques et de Jeunesse. Je réalise également des sites web pour des PME qui me sollicitent pour la création de leur site web.</p>
-            <p>Vous avez un projet web qui vous tient à cœur? N'hésitez pas à me contacter pour en discuter et voir comment je peux vous aider à le concrétiser.</p>
+            <p>Vous avez un projet web qui vous tient à cœur? <a href="mailto:contact@digitalgarage.ch">N'hésitez pas à me contacter</a> pour en discuter et voir comment je peux vous aider à le concrétiser.</p>
           </div>
         </div>
         <h3 className="mt-20 sm:mb-10">Dernières réalisations</h3>
